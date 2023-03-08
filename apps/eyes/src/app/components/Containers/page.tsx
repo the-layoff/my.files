2a48d817ba5285a411ea6fa7e4c8d8d5a1324e64
techdeus <sullivan.marlon@gmail.com>
2023-03-08 00:10:04 -0800

    Update package.json and check chromatic

--- apps/eyes/src/app/components/Containers/page.tsx
+++ apps/eyes/src/app/components/Containers/page.tsx
@@ -1,22 +1,22 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
 export interface ContainerProps {
-  id?: string;
-  children?: ReactNode;
+	id?: string;
+	children?: ReactNode;
 }
 
 const PageContainer: React.FC<ContainerProps> = ({
-  id,
-  children,
+	id,
+	children,
 }: ContainerProps): JSX.Element => {
-  return (
-    <div
-      id={id ?? undefined}
-      className="container mf-grid mf-min-h-screen mf-min-w-full mf-grid-cols-2 mf-gap-0"
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			id={id ?? undefined}
+			className='mf-grid mf-min-h-screen mf-min-w-full mf-grid-cols-2 mf-gap-0 mf-w-0.5 container'
+		>
+			{children}
+		</div>
+	);
 };
 
 export default PageContainer;
